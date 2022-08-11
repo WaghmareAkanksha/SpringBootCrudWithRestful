fa2ac30542d5459a4f29d024050cd845fa3b7fc3
bhagwat <bhagwat0and1@gmail.com>
2022-08-11 18:32:23 +0530

    SPRIN-2 : create entity class

--- src/main/java/com/Infosys/SpringBootCrudWithRestful/model/Employee.java
+++ src/main/java/com/Infosys/SpringBootCrudWithRestful/model/Employee.java
@@ -1,5 +1,62 @@
 package com.Infosys.SpringBootCrudWithRestful.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String userName;
+	private String password;
+	private long salary;
+	private String designation;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", userName=" + userName + ", password=" + password
+				+ ", salary=" + salary + ", designation=" + designation + "]";
+	}
+	
 }
