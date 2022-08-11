28aa713708ba873fd75686bde3438197d0f1da76
bhagwat <bhagwat0and1@gmail.com>
2022-08-11 18:52:50 +0530

    SPRIN-4 : create servicei interface

--- src/main/java/com/Infosys/SpringBootCrudWithRestful/service/EmployeeService.java
+++ src/main/java/com/Infosys/SpringBootCrudWithRestful/service/EmployeeService.java
@@ -1,5 +1,22 @@
 package com.Infosys.SpringBootCrudWithRestful.service;
 
+import java.util.List;
+
+import com.Infosys.SpringBootCrudWithRestful.model.Employee;
+
 public interface EmployeeService {
+
+	
+	public int saveUserServic(Employee employee);
+
+	public List<Employee> getAllUserServices();
+	
+	public Employee loginCheck(String userName,String password);
+
+	public Employee updateUser(Employee employee);
+	
+	public String deleteEmployee(int id);
+
+
 
 }
--- src/main/java/com/Infosys/SpringBootCrudWithRestful/service/EmployeeServiceImpl.java
+++ src/main/java/com/Infosys/SpringBootCrudWithRestful/service/EmployeeServiceImpl.java
@@ -1,5 +1,45 @@
 package com.Infosys.SpringBootCrudWithRestful.service;
 
-public class EmployeeServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.Infosys.SpringBootCrudWithRestful.dao.EmployeeDao;
+import com.Infosys.SpringBootCrudWithRestful.model.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Override
+	public int saveUserServic(Employee employee) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Employee> getAllUserServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee loginCheck(String userName, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee updateUser(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteEmployee(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
