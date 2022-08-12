d3de2fd49af479844d9951c4c9e6a37c13bb3492
bhagwat <bhagwat0and1@gmail.com>
2022-08-12 12:44:51 +0530

    SPRIN-13 : provice implementaion for controller layer

--- src/main/java/com/Infosys/SpringBootCrudWithRestful/controller/EmployeeController.java
+++ src/main/java/com/Infosys/SpringBootCrudWithRestful/controller/EmployeeController.java
@@ -1,5 +1,53 @@
 package com.Infosys.SpringBootCrudWithRestful.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Infosys.SpringBootCrudWithRestful.model.Employee;
+import com.Infosys.SpringBootCrudWithRestful.service.EmployeeService;
+
+@RestController
 public class EmployeeController {
 
+	@Autowired
+	private EmployeeService employeeService;
+
+	public ResponseEntity<Integer> saveUser(@RequestBody Employee employee) {
+
+		int saveUserServic = employeeService.saveUserServic(employee);
+		return new ResponseEntity<>(saveUserServic, HttpStatus.CREATED);
+
+	}
+
+	public ResponseEntity<List<Employee>> getAll() {
+		List<Employee> allUserServices = employeeService.getAllUserServices();
+
+		return new ResponseEntity<List<Employee>>(allUserServices, HttpStatus.OK);
+
+	}
+
+	public ResponseEntity<Employee> loginCheck(@PathVariable String name, @PathVariable String password) {
+
+		Employee loginCheck = employeeService.loginCheck(name, password);
+
+		if (loginCheck == null) {
+			return new ResponseEntity<>(loginCheck, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(loginCheck, HttpStatus.FOUND);
+	}
+
+	public ResponseEntity<String> deletId(@PathVariable int id) {
+
+		String deleteEmployee = employeeService.deleteEmployee(id);
+
+		return new ResponseEntity<String>(deleteEmployee, HttpStatus.OK);
+
+	}
 }
