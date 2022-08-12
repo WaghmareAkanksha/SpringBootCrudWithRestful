0386d59c31ee6fa458ebb60f105a9739101d626b
Your Name <shendepoonam1997@gmail.com>
2022-08-12 11:41:37 +0530

    SPRIN -11 :declare mehtod in daoI

--- src/main/java/com/Infosys/SpringBootCrudWithRestful/dao/EmployeeDao.java
+++ src/main/java/com/Infosys/SpringBootCrudWithRestful/dao/EmployeeDao.java
@@ -1,5 +1,20 @@
 package com.Infosys.SpringBootCrudWithRestful.dao;
 
-public interface EmployeeDao {
+import java.util.List;
 
+import com.Infosys.SpringBootCrudWithRestful.model.Employee;
+
+public interface EmployeeDao 
+{
+    public int saveUserDao();
+    
+    public List<Employee> getAllUserDao();
+    
+    public Employee loginCheckDao(String userName,String password);
+
+	public Employee updateUserDao(Employee employee);
+	
+	public String deleteEmployeeDao(int id);
+
+    
 }
